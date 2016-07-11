3b7476dfd23379f7295ae131cad47be3cca3ec2a
Dan Pock <dpock32509@gmail.com>
2016-07-11 01:47:35 -0400

    Move everything into laravel

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,32 @@
 */
 
 Route::get('/', function () {
-    return view('default');
+    //config vars
+    $image_directory = "images";
+    $thumbnail_directory = "thumbs";
+    $data = [];
+    $data['files'] = [];
+
+    //Check for and create directory for images and thumbnails
+    if (!is_dir("$image_directory")) {
+        mkdir("$image_directory", 0755, true);
+    };
+    if (!is_dir("$thumbnail_directory")) {
+    	mkdir("$thumbnail_directory", 755, true);
+    };
+
+    //Setup directory for images
+    $directory = "$image_directory";
+    //read through image directory and get rid of .. , . , .htaccess , and .ftpquota
+    $scanned_directory = array_diff(scandir($directory), array('..', '.', '.htaccess', '.ftpquota'));
+    $data['files'] = $scanned_directory;
+
+    foreach($scanned_directory as $file) {
+    	//check for and create thumbnails
+    	createthumb($image_directory."/".$file,$thumbnail_directory."/".$file,30000,300);
+    }
+
+    return view('default', $data);
 });
 
 Route::get('/welcome', function () {
