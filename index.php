aad4e3e165b49a15678935e55f2d382bbe2160e0
root <root@digi.thrasherht.com>
2016-07-22 23:58:46 -0400

    Fix the way headers and footers are loaded, as to use less if statements in main code

--- index.php
+++ index.php
@@ -22,18 +22,12 @@ if (!is_dir("$thumbnail_directory")) {
     mkdir("$thumbnail_directory", 0755, true);
 };
 
-//Opening div tags
-if ( use_bootstrap_gallery ){
-	print "<div id=\"links\">";
-} else {
-	print "<div class=\"container\">\n";
-	print "<div class=\"row\">\n";
-}
 //Setup directory for images
 $directory = "$image_directory";
 //read through image directory and get rid of .. , . , .htaccess , and .ftpquota
 $scanned_directory = array_diff(scandir($directory), array('..', '.', '.htaccess', '.ftpquota'));
 //loop through all the files from the directory listing
+
 foreach($scanned_directory as $file) {
   //do your work here
 	//check for and create thumbnails
@@ -42,10 +36,6 @@ foreach($scanned_directory as $file) {
 	print '<a href="images/'.$file.'" data-lightbox="Snips Gallery"><div class="col-sm-12 col-md-3 col-lg-2"><div class="logo-box" style="background-image:url(thumbs/'.$file.');" ></div></div></a>';
 	print "\n";
 }
-print '
-</div>
-</div>
-';
 
 //Footer include
 //Check if bootstrap gallery is enabled
