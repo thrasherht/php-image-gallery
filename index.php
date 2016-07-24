afb4fbd2973b9d6f12e1c0a3dd80a5729a84f33e
root <root@digi.thrasherht.com>
2016-07-24 03:31:48 -0400

    Adding some CSS to make it look nicer

--- index.php
+++ index.php
@@ -24,9 +24,9 @@ $scanned_directory = array_diff(scandir($directory), array('..', '.', '.htaccess
 foreach($scanned_directory as $file) {
   //do your work here
 	//check for and create thumbnails
-	createthumb("$image_directory/$file","$thumbnail_directory/$file",30000,300);
+	createthumb("$image_directory/$file","$thumbnail_directory/$file",200,200);
 	//Create HTML code for each image
-	print '<a href="'.$image_directory.'/'.$file.'" title="'.$file.'" class="lightbox_trigger"><img src="'.$thumbnail_directory.'/cropped/'.$file.'"></a>';
+	print '<a href="'.$image_directory.'/'.$file.'" title="'.$file.'" class="lightbox_trigger"><img src="'.$thumbnail_directory.'/'.$file.'"></a>';
 	print "\n"; }
 
 //Footer include
