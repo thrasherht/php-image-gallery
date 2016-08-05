1ad302bd275fc700bd2d75c5f7c2d917e8c5178e
Tayler Sewell <tsewell@liquidweb.com>
2016-08-04 21:08:08 -0400

    Cleaned up div display and resizer to make better thumbs

--- index.php
+++ index.php
@@ -24,10 +24,11 @@ $scanned_directory = array_diff(scandir($directory), array('..', '.', '.htaccess
 foreach($scanned_directory as $file) {
   //do your work here
 	//check for and create thumbnails
-	createthumb("$image_directory/$file","$thumbnail_directory/$file",200,20000);
+	createthumb("$image_directory/$file","$thumbnail_directory/$file",200,200);
 	//Create HTML code for each image
-	print '<a href="'.$image_directory.'/'.$file.'" title="'.$file.'" class="lightbox_trigger"><img src="'.$thumbnail_directory.'/'.$file.'"></a>';
-	print "\n"; }
+	print '<a href="'.$image_directory.'/'.$file.'" title="'.$file.'" class="lightbox_trigger"><div class="imagetile" style="background: url('.$thumbnail_directory.'/'.$file.') no-repeat;"></div></a>';
+	print "\n";
+}
 
 //Footer include
 //Check if bootstrap gallery is enabled
